d8604793d9467dbeb309ebe186e8807536ffb00b
Saglara <nohaevass@yandex.ru>
2023-08-06 13:12:26 +0300

    1

--- ex2.py
+++ ex2.py
@@ -2,3 +2,18 @@
 # ✔ Имя должно начинаться с заглавной буквы, состоять из 4-7 букв,
 # среди которых обязательно должны быть гласные.
 # ✔ Полученные имена сохраните в файл.
+import string
+import random
+
+vowel = 'аеёиоуэюя'
+consonant = 'бвгджзйкдмнлнпрстфхцчшщъь'
+# print(string.ascii_lowercase)
+
+a = random.randint(4, 7)
+v = random.randint(1, a-2)
+s = random.sample(vowel, a) + random.sample(consonant, a-v)
+
+
+print(x)
+print(y)
+print(s)
